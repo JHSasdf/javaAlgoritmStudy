4d4ae6c41e6881080840d5a4d5ea82ec3155dcee
JHSasdf <sgh4760@naver.com>
2023-11-09 22:29:37 +0900

    Study:231109

--- method.java
+++ method.java
@@ -18,11 +18,40 @@ public class method {
             System.out.println((int) Math.pow(i,2));
         }
     }
-    public static void main(String[] args) {
 
-        helloWorldTwice(1,2);
-        helloWorldTwice(1,2,845);
-        sayHelloWorld(5);
-        printSquares(5);
+    public static void print() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%d * %d = %d\n", 5, i, 5*i);
+        }
     }
+    public static void printMultyTable(int num) {
+
+        for (int j = 1; j <= 9; j++) {
+            System.out.printf("%d * %d = %d, ", num, j, num * j);
+        }
+        System.out.println();
+    }
+
+    public static void printMultyTable(int num1, int num2) {
+        for (int j = 1; j <= 9; j++) {
+            System.out.printf("%d * %d = %d, ", num1, j, num1 * j);
+        }
+        System.out.println();
+
+        for (int j = 1; j <= 9; j++) {
+            System.out.printf("%d * %d = %d, ", num2, j, num2 * j);
+        }
+        System.out.println();
+    }
+
+    public  static int getLastAngle(int num1, int num2) {
+        if (num1 + num2 >= 180) {
+            System.out.println("it's not a triangle");
+            return 0;
+        } else {
+            return 180 - num1 -num2;
+        }
+
+    }
+
 }
--- multiplication_Table_01.java
+++ multiplication_Table_01.java
@@ -2,43 +2,17 @@
 package UDEMY_JAVA.javaWorkSpace;
 
 public class multiplication_Table_01 {
-    public static void printMultyTable(int num) {
 
-        for (int j = 1; j <= 9; j++) {
-            System.out.printf("%d * %d = %d, ", num, j, num * j);
-        }
-        System.out.println();
-    }
 
-    public static void printMultyTable(int num1, int num2) {
-        for (int j = 1; j <= 9; j++) {
-            System.out.printf("%d * %d = %d, ", num1, j, num1 * j);
-        }
-        System.out.println();
-
-        for (int j = 1; j <= 9; j++) {
-            System.out.printf("%d * %d = %d, ", num2, j, num2 * j);
-        }
-        System.out.println();
-    }
-
-    public  static int getLastAngle(int num1, int num2) {
-        if (num1 + num2 >= 180) {
-            System.out.println("it's not a triangle");
-            return 0;
-        } else {
-            return 180 - num1 -num2;
-        }
-
-    }
 
     public static void main(String[] args) {
-        printMultyTable(3);
-        printMultyTable(7, 8);
-        System.out.println(getLastAngle(12, 180));
 
-//        클래스 살펴보기
-        multiplication_Table_01 earth = new multiplication_Table_01();
-        System.out.println(earth.getLastAngle(70,40));
+        method table = new method();
+
+        table.print();
+        table.helloWorldTwice(3,4);
+        table.helloWorldTwice(3,4,7);
+
+
     }
 }
