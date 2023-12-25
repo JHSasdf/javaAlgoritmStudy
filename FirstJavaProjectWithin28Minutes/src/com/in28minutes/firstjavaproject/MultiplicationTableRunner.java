ead39571d1b47fa0a18dd808f8812fe0725e0b7e
선지훈 <sgh4760@naver.com>
2023-12-25 17:15:14 +0900

    study:231225

---
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -0,0 +1,17 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+		table.print(6);
+
+		// public method로 설정했음
+//		for (int i = 2; i < 10; i++) {			
+//			MultiplicationTable.print(i);
+//		}
+
+		table.print(5, 11, 20);
+	}
+}
